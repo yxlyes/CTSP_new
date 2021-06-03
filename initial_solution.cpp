4fcb3d5885336b85c188b4b567b1ca89e8dc1e30
yang2110862 <1563782600@qq.com>
2021-06-03 21:52:39 +0800

    initial_solution

--- initial_solution.cpp
+++ initial_solution.cpp
@@ -9,14 +9,16 @@ extern vector<vector<double>> dist;
 void initial_solution(solution& sol) {
     vector<int> node;
     int pointer;  
+    int num;
+    int rand1, rand2, temp;
+    double delta, delta_temp;
     //the first step : build partial route
     for (int i = 0; i < num_salesman; ++i) {
         for (int j = 1; j <= num_city; ++j) {
             if(city_set[j] == (i + 1)) node.push_back(j);
         }
-        int num = node.size();
+        num = node.size();
         //打乱节点顺序
-        int rand1, rand2, temp;
         for (int j = 0; j < num; ++j) {
             rand1 = rand() % num;
             rand2 = rand() % num;
@@ -36,10 +38,9 @@ void initial_solution(solution& sol) {
         //remaing cities insert greedily and probabilistically
         int loc;
         while (node.size()) {
-            double delta = DBL_MAX;
+            delta = DBL_MAX;
             num = node.size();
             pointer = 0;
-            double delta_temp;
             while (1) {
                 delta_temp = dist[pointer][node[0]] + dist[node[0]][sol.solution[i][pointer]] - dist[pointer][sol.solution[i][pointer]];
                 if (delta_temp < delta) {
@@ -56,12 +57,48 @@ void initial_solution(solution& sol) {
         }
     }         
     //the second step : dispatch the shared cities
-#ifdef DEBUG
+    for (int i = 1; i <= num_city; ++i) {
+        if(city_set[i] == 0) node.push_back(i);
+    }
+    num = node.size();
+    //int pos, row
+    int pos = 0, row = 0;
+    while (node.size()) {
+        delta = DBL_MAX;
+        for (int i = 0; i < num_salesman; ++i) {
+            pointer = 0;
+            while (1) {
+                delta_temp = dist[pointer][node[0]] + dist[node[0]][sol.solution[i][pointer]] - dist[pointer][sol.solution[i][pointer]];
+                if (delta_temp < delta) {
+                    delta = delta_temp;
+                    pos = pointer;
+                    row = i;
+                }
+                pointer = sol.solution[i][pointer];
+                if (pointer == 0) break;
+            }
+        }
+        temp = sol.solution[row][pos];
+        sol.solution[row][pos] = node[0];
+        sol.solution[row][node[0]] = temp;
+        node.erase(node.begin());
+    }
+    //compute cost
+    for (int i = 0; i < num_salesman; ++i) {
+        pointer = 0;
+        while (1) {
+            sol.cost += dist[pointer][sol.solution[i][pointer]];
+            pointer = sol.solution[i][pointer];
+            if (pointer == 0) break;
+        }
+    }
+#ifdef DEBUG1
     for (int i = 0; i < sol.solution.size(); ++i) {
         for (auto x : sol.solution[i]) {
             cout << x << ' ';
         }
         cout << endl;
     }
+    cout << sol.cost << endl;
 #endif
 }
